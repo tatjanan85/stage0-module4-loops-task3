d63b067911b8511a9ce58d91f8431103c576dbcf
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2023-12-27 14:16:26 +0300

    fix: fix gcd task

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -3,12 +3,17 @@ package school.mjc.stage0.loops.task3;
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
         int gcd = 1;
-        for(int i = 2; i <= first; i++) {
+        int greatest = first > second ? first : second;
+        for(int i = 2; i <= greatest; i++) {
             if (first % i == 0 && second % i == 0) {
                 gcd = i;
             }
         }
 
         System.out.println(gcd);
+    }
+
+    public static void main(String[] args) {
+        new GreatestCommonDivisor().printGCD(15, 30);
     }
 }
