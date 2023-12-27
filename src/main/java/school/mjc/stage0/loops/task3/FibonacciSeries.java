b59d1a4af107b3ea863a7df414035ad9d5f457a0
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2023-12-27 14:13:26 +0300

    fix: fix fibonacci task

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,14 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
-        int sum = 0, n1 = 0, n2 = 1;
+        int sum = 1, n1 = 0, n2 = 1;
         System.out.println(n1);
 
-        if (lastFibonacci == 2) {
-            sum = 1;
-        }
-
-        if (lastFibonacci > 2) {
+        if (lastFibonacci > 1) {
             System.out.println(sum);
             for (int i = 3; i <= lastFibonacci; i++) {
                 sum = n1 + n2;
